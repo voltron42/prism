cf63b179fc444062977429117fc48d03a48a00f0
Daniel Johnson <daniel.johnson@surescripts.com>
2015-05-27 16:37:55 -0500

    adding xml unmarshalling for RGB with test

--- factory.go
+++ factory.go
@@ -1,6 +1,10 @@
 package prism
 
-import "image/color"
+import (
+	"encoding/xml"
+	"image/color"
+	"strconv"
+)
 
 type ColorFactory interface {
 	GetColor(id string) color.Color
@@ -16,4 +20,23 @@ func (c *RGB) RGBA() (r, g, b, a uint32) {
 	b = c.B
 	a = 255
 	return
+}
+
+func (rgb *RGB) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	for _, attr := range start.Attr {
+		num, err := strconv.Atoi(attr.Value)
+		if err != nil {
+			return err
+		}
+		switch attr.Name.Local {
+		case "red":
+			rgb.R = uint32(num)
+		case "green":
+			rgb.G = uint32(num)
+		case "blue":
+			rgb.B = uint32(num)
+		}
+	}
+	d.Token()
+	return nil
 }
--- factory_test.go
+++ factory_test.go
@@ -2,12 +2,28 @@ package prism_test
 
 import (
 	"./"
+	"encoding/xml"
 	"fmt"
 	"testing"
 )
 
 func Test(t *testing.T) {
 	color := prism.RGB{R: 1, G: 2, B: 3}
+
+	r, g, b, a := color.RGBA()
+
+	fmt.Printf("red: %v, green: %v, blue: %v, alpha %v\n", r, g, b, a)
+}
+
+func Test_xml(t *testing.T) {
+	color := prism.RGB{}
+	bytes := []byte("<rgb red=\"1\" green=\"2\" blue=\"3\"/>")
+
+	err := xml.Unmarshal(bytes, &color)
+
+	if err != nil {
+		t.Error(err)
+	}
 
 	r, g, b, a := color.RGBA()
 
